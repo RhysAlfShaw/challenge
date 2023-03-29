e63b7159f3148cf2e288d7cbfefb26231106a7fc
Rhys Shaw <rhysc3p0@gmail.com>
2023-03-29 11:20:05 +0100

    added download script

---
+++ bin/download_data.sh
@@ -0,0 +1,2 @@
+mkdir data
+curl -L -o data/survey.csv https://energydata.info/dataset/a27a9b60-706b-4c81-8608-c913d2ed998f/resource/fdef4f22-fe57-49b1-9c42-e5dac79cc90c/download/pakistanbiomassfieldsurvey.csv
